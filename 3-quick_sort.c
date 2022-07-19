c8ac9b4d402ad15fd2bf1879940b555db9661528
BernieW <87444402+Berniac@users.noreply.github.com>
2022-07-20 00:39:55 +0300

    Update 3-quick_sort.c

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,58 +1,84 @@
 #include "sort.h"
 
 /**
- * quick_sort - function that sorts an array
- * of integers in ascending order using the
- * quick sort algorithm
+ * swap - change position between two position of an array and print the array
+ * @array: array to sort
+ * @first: lowest position
+ * @last: highest position
+ * @size: size of array
  *
- * @array: input arrray
- * @size: size of the array
- * Return: no return
+ * Return: Void
+ */
+void swap(int *array,  int first, int last, size_t size)
+{
+	int aux, value;
+
+	value = array[first];
+	aux = array[last];
+	array[last] = value;
+	array[first] = aux;
+	print_array(array, size);
+}
+/**
+ * part - fin the partition position or pivot of the array
+ * @array: array to sort
+ * @first: lowest position
+ * @last: highest position
+ * @size: size of array
+ * Return: pivot index
+ */
+int part(int *array, int first, int last, size_t size)
+{
+	int pivot = array[last];
+	int i = first - 1;
+	int j;
+
+	for (j = first; j <= last - 1; j++)
+	{
+		if (array[j] < pivot)
+		{
+			i++;
+			if (i < j)
+				swap(array, i, j, size);
+		}
+	}
+	if (array[i + 1] > array[last])
+		swap(array, i + 1, last, size);
+	return (i + 1);
+}
+
+/**
+ * sorting - sorts an array of integers in ascending order
+ * @array: array to sort
+ * @first: lowest position
+ * @last: highest position
+ * @size: size of array
+ *
+ * Return: Void
+ */
+void sorting(int *array, int first, int last, size_t size)
+{
+	int pivot;
+
+	if (first < last)
+	{
+		pivot = part(array, first, last, size);
+		sorting(array, first, pivot - 1, size);
+		sorting(array, pivot + 1, last, size);
+	}
+}
+
+/**
+ * quick_sort - sorts an array of integers in ascending order using the Quick
+ * sort algorithm using Lomuto partition scheme
+ * @array: array to sort
+ * @size: size of array
+ *
+ * Return: Void.
  */
 void quick_sort(int *array, size_t size)
 {
-	_qsort(array, 0, size - 1, size);
-}
-/**
- * _qsort - auxiliar function for the
- * quick_sort function
- * @a: input arrray
- * @low: index for the first element
- * @high: index for the last element
- * @size: size of the array
- * Return: no return
- */
-void _qsort(int *a, int low, int high, int size)
-{
-	int p, w, i;
-	int tmp;
+	int last = size - 1;
 
-	if (low < high)
-	{
-		p = high;
-		w = low;
-		for (i = low; i < high; i++)
-		{
-			if (a[i] < a[p])
-			{
-				if (i != w)
-				{
-					tmp = a[i];
-					a[i] = a[w];
-					a[w] = tmp;
-					print_array(a, size);
-				}
-				w++;
-			}
-		}
-		if (w != p && a[w] != a[p])
-		{
-			tmp = a[w];
-			a[w] = a[p];
-			a[p] = tmp;
-			print_array(a, size);
-		}
-		_qsort(a, low, w - 1, size);
-		_qsort(a, w + 1, high, size);
-	}
+	sorting(array, 0, last, size);
 }
